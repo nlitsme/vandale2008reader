28ba870224437d34da343f0e6c8e1dbb05f3d92e
itsme <itsme@xs4all.nl>
2023-02-19 09:20:24 +0100

    updated itslib and cmake structure

--- cmake_find/Finditslib.cmake
+++ cmake_find/Finditslib.cmake
@@ -1,33 +1,15 @@
-# cmake package finder for itslib
-
-file(GLOB ITSLIB_DIRS itslib  submodules/itslib ../itslib ../../itslib)
-# first check for the local-copy itslib variant
-find_path(ITSLIB_PATH NAMES incxxxlude/stringutils.h PATHS ${ITSLIB_DIRS})
-if(NOT ITSLIB_PATH STREQUAL "ITSLIB_PATH-NOTFOUND")
-	set(ITSLIB_INCLUDE_DIR ${ITSLIB_PATH}/include)
+if (TARGET itslib)
+    return()
+endif()
+find_path(ITSLIB_PATH NAMES include/itslib/stringutils.h PATHS symlinks/itslib)
+if(ITSLIB_PATH STREQUAL "ITSLIB_PATH-NOTFOUND")
+    include(FetchContent)
+    FetchContent_Populate(itslib
+        GIT_REPOSITORY https://github.com/nlitsme/legacy-itslib-library)
+    set(ITSLIB_PATH  ${itslib_SOURCE_DIR})
 else()
-	# then check for the repository itslib variant
-	find_path(ITSLIB_PATH NAMES include/itslib/stringutils.h PATHS ${ITSLIB_DIRS})
-	if(NOT ITSLIB_PATH STREQUAL "ITSLIB_PATH-NOTFOUND")
-		set(ITSLIB_INCLUDE_DIR ${ITSLIB_PATH}/include/itslib)
-	else()
-		include(FetchContent)
-		FetchContent_Populate(itslib
-			GIT_REPOSITORY https://github.com/nlitsme/legacy-itsutils-library)
-		set(ITSLIB_PATH ${CMAKE_BINARY_DIR}/itslib-src)
-		set(ITSLIB_INCLUDE_DIR ${ITSLIB_PATH}/include/itslib)
-    endif()
+    set(itslib_BINARY_DIR ${CMAKE_BINARY_DIR}/itslib-build)
 endif()
 
-message(INFO " Using itslib from ${ITSLIB_PATH}")
-
-list(APPEND ITSLIBSRC debug.cpp stringutils.cpp utfcvutils.cpp vectorutils.cpp)
-list(TRANSFORM ITSLIBSRC PREPEND ${ITSLIB_PATH}/src/)
-add_library(itslib STATIC ${ITSLIBSRC})
-target_include_directories(itslib PUBLIC ${ITSLIB_INCLUDE_DIR})
-target_compile_definitions(itslib PUBLIC _NO_RAPI _NO_WINDOWS)
-if(NOT WIN32)
-    target_compile_definitions(itslib PUBLIC _UNIX)
-endif()
-
+add_subdirectory(${ITSLIB_PATH})
 
--- cmake_find/Findzlib.cmake
+++ cmake_find/Findzlib.cmake
@@ -1,8 +1,8 @@
 include(FetchContent)
 FetchContent_Populate(zlib
     # TODO: we need a method of supplying a local cache of this file.
-    URL http://www.zlib.net/zlib-1.2.11.tar.gz
-    URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+    URL http://www.zlib.net/zlib-1.2.13.tar.gz
+    URL_HASH SHA256=b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30
 )
 set(ZLIBDIR ${CMAKE_BINARY_DIR}/zlib-src)
 
