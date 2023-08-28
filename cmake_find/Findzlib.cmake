d8839efa4741317ec2197ac05ab0451386f0de8b
itsme <itsme@xs4all.nl>
2023-08-28 10:49:40 +0200

    fixed portability issues

--- cmake_find/Findzlib.cmake
+++ cmake_find/Findzlib.cmake
@@ -1,13 +1,13 @@
 include(FetchContent)
 FetchContent_Populate(zlib
     # TODO: we need a method of supplying a local cache of this file.
-    URL http://www.zlib.net/zlib-1.2.13.tar.gz
-    URL_HASH SHA256=b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30
+    URL https://www.zlib.net/zlib-1.3.tar.gz
+    URL_HASH SHA256=ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e
 )
 set(ZLIBDIR ${CMAKE_BINARY_DIR}/zlib-src)
 
 list(APPEND ZLIBFILES inftrees.c infback.c compress.c inffast.c adler32.c uncompr.c inflate.c deflate.c crc32.c trees.c zutil.c)
-list(APPEND ZLIBFILES gzclose.c gzread.c gzwrite.c gzlib.c)
+#list(APPEND ZLIBFILES gzclose.c gzread.c gzwrite.c gzlib.c)  <-- build failure on macos because of missing include <unistd.h>
 list(TRANSFORM ZLIBFILES PREPEND ${ZLIBDIR}/)
 add_library(zlib ${LIBSTYLE} ${ZLIBFILES})
 target_include_directories(zlib PUBLIC ${ZLIBDIR})
